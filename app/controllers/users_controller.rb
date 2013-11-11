81403ce92bebefb0c1c2d2fb5275580ccd8208eb
Jun Lim <junyuen@gmail.com>
2013-11-12 03:45:50 +1100

    Add User Show Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,5 +17,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @projects = @user.projects
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActiveadminTest::Application.routes.draw do
 
+  get "users/show"
+
   resources :projects do
       member do
       get :followers
@@ -17,6 +19,7 @@ ActiveadminTest::Application.routes.draw do
   #devise_for :admin_users, ActiveAdmin::Devise.config
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  match 'users/:id' => 'users#show', as: :user
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
