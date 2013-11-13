df82152f4fefc62d36ce31dfb10ed7b388f5bfbb
Jun Lim <junyuen@gmail.com>
2013-11-14 00:26:47 +1100

    Migration ChangeDescriptionStringToText

---
+++ db/migrate/20131113132318_change_description_string_to_text.rb
@@ -0,0 +1,9 @@
+class ChangeDescriptionStringToText < ActiveRecord::Migration
+  def up
+  	change_column :projects, :description, :text
+  end
+
+  def down
+  	change_column :projects, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131113110613) do
+ActiveRecord::Schema.define(:version => 20131113132318) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(:version => 20131113110613) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
-    t.string   "description"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.text     "description",            :limit => 255
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.integer  "user_id"
     t.string   "thumbnail_file_name"
     t.string   "thumbnail_content_type"
