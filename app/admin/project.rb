af0a6ef6a1d6c8a62d9a42e1de8e7ba2fe1e0a43
Jun Lim <junyuen@gmail.com>
2013-11-21 16:11:06 +0800

    Edit Projects Views

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -11,4 +11,17 @@ ActiveAdmin.register Project do
     default_actions
   end
 
+  form do |f|
+    f.inputs "User Details" do
+        f.input :id
+        f.input :title
+        f.input :description
+        f.input :city_list
+        f.input :tag_list
+        f.input :user
+        f.input :published
+    end
+    f.buttons
+  end
+
 end
