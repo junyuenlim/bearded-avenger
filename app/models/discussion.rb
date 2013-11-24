5e07692d9f0044b526b45bd6da77770f6cdce8f7
Jun Lim <junyuen@gmail.com>
2013-11-25 02:06:11 +0800

    Edit Discussions Index views

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -9,8 +9,9 @@ class Discussion < ActiveRecord::Base
   acts_as_taggable_on :city
 
   validates :user_id, presence: true
-  has_attached_file :cover, styles: { medium: ["1400x600>", :jpg], thumb: ["560x400#", :jpg] }
-  validates_attachment :cover, presence: true,
+  has_attached_file :cover, styles: { medium: ["1400x600>", :jpg], thumb: ["560x400#", :jpg] },
+                    default_url: '/assets/graphics/missing_thumbnail.jpg'
+  validates_attachment :cover,
                         content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                         size: { less_than: 5.megabytes }
 
