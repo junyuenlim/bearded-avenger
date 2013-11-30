59aab0029a09643e61555479fb1c8bccf304ad84
Jun Lim <junyuen@gmail.com>
2013-12-01 00:19:05 +0800

    ErrorFix application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,14 +39,6 @@ $(document).ready(function () {
   $("div[id^='flash-message']").fadeOut(3000);
 });
 
-$(document).ready(function() {
- var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-}
 
 $("#tooltip span").tooltip()
 $("#followers a").tooltip()
